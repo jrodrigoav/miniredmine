3dfa2254b8970400e5a60c424b160f0f6c6192ca
Jesus Acedo <jrodrigoav@hotmail.com>
2021-01-25 22:56:22 -0600

    Dammit heroku

--- MiniRedmine.Web/Models/UnosquareSettings.cs
+++ MiniRedmine.Web/Models/UnosquareSettings.cs
@@ -2,11 +2,5 @@
 
 namespace MiniRedmine.Web.Models
 {
-    public class UnosquareLeads : List<TeamLead> { }
-
-    public class TeamLead
-    {
-        public string Name { get; init; }
-        public int UserId { get; init; }
-    }
+    public class UnosquareLeads : List<int> { }
 }
