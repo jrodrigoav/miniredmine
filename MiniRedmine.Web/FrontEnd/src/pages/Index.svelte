d7ccbf83ce6be9868f34a7b9f4b3ea44e0df226c
Jesus Acedo <jrodrigoav@hotmail.com>
2021-02-24 21:26:06 -0600

    Add crocodile and unicorn

--- MiniRedmine.Web/FrontEnd/src/pages/Index.svelte
+++ MiniRedmine.Web/FrontEnd/src/pages/Index.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="jumbotron">
-  <h1 class="display-3">MiniRedmine, Svelte!</h1>
+  <h1 class="display-3">🐊 MiniRedmine, Svelte! 🦄</h1>
   {#if $user.unauthorized === true}
     <p class="lead">
       Welcome you need to register your user info to use the app.
