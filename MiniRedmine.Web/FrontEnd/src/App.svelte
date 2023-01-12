01131c997afb9bbc9fab5a00ca80d3c0d252a728
Rodrigo <jrodrigoav@gmail.com>
2023-01-11 21:05:11 -0600

    Update to 2.4.1 (#32)
    * Update to 2.4.1
    
    * Yep
    
    Co-authored-by: Jesus Acedo <jrodrigoav@hotmail.com>

--- MiniRedmine.Web/FrontEnd/src/App.svelte
+++ MiniRedmine.Web/FrontEnd/src/App.svelte
@@ -94,5 +94,5 @@
   <Index />
 {/if}
 <footer>
-  Copyright &copy; Jesus Acedo 2018 - 2022 version: 2.4.0
+  Copyright &copy; Jesus Acedo 2018 - 2023 version: 2.4.1
 </footer>
--- devops/RunImage.ps1
+++ devops/RunImage.ps1
@@ -1,1 +1,2 @@
-docker run --detach -p 62474:80 jrodrigoav/miniredmine:2.4.0
+$leads="-3,-2,-1,0";
+docker run --detach --publish 62474:80 --env UNOSQUARE__LEADS=$leads --env ASPNETCORE_ENVIRONMENT="Production" jrodrigoav/miniredmine:2.4.1
