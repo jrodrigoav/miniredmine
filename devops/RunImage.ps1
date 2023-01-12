74e10d23670540d4bd47b2ed0662d2880baf5414
Jesus Acedo <jrodrigoav@hotmail.com>
2023-01-11 21:05:04 -0600

    Yep

--- devops/RunImage.ps1
+++ devops/RunImage.ps1
@@ -1,1 +1,2 @@
-docker run --detach --publish 62474:80 --env ASPNETCORE_ENVIRONMENT="Production" jrodrigoav/miniredmine:2.4.1
+$leads="-3,-2,-1,0";
+docker run --detach --publish 62474:80 --env UNOSQUARE__LEADS=$leads --env ASPNETCORE_ENVIRONMENT="Production" jrodrigoav/miniredmine:2.4.1
