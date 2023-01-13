43858ca3ba7c2d4558f42e634183e737a4301c65
Jesus Acedo <jrodrigoav@hotmail.com>
2023-01-12 23:02:34 -0600

    meh

--- devops/RunImage.ps1
+++ devops/RunImage.ps1
@@ -1,3 +1,3 @@
-$leads="-3,-2,-1,0,627";
-$redmineurl="https://dev.unosquare.com/redmine/";
+$leads="-3,-2,-1,0";
+$redmineurl="";
 docker run --detach --publish 62474:80 --env Unosquare__RedmineUrl=$redmineurl --env Unosquare__Leads=$leads --env ASPNETCORE_ENVIRONMENT="Development" jrodrigoav/miniredmine:2.4.2
