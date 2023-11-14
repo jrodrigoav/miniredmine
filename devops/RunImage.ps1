a8167385051f42ea45f9e28fa6fd2bb5e271ceb0
Jesus Acedo <jrodrigoav@hotmail.com>
2023-11-14 16:08:14 -0600

    Upgrade to .NET 8

--- devops/RunImage.ps1
+++ devops/RunImage.ps1
@@ -1,2 +1,2 @@
-$redmineurl="";
+$redmineurl=Read-Host -Promt "Redmine URL";
 docker run --detach --publish 62474:80 --env Unosquare__RedmineUrl=$redmineurl --env ASPNETCORE_ENVIRONMENT="Development" jrodrigoav/miniredmine:2.5.1
